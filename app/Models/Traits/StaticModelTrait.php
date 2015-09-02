06477ef677706975ebf71676525a1253fd83309b
Aleksandar Nikolic <sasa.n@vivifyideas.com>
2015-09-02 10:21:52 +0200

    Adding static model example and trait. #LAB-13

---
+++ app/Models/Traits/StaticModelTrait.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App;
+
+trait StaticModelTrait 
+{
+    protected static $defaultKeyField;
+    protected static $defaultValueField;
+    protected static $arrayKeys;
+
+    public static function all()
+    {
+        return self::$items;
+    }
+
+    public static function first()
+    {
+        return current(self::$items);
+    }
+
+    public static function getByKey($key)
+    {
+        $keyField = isset(self::$keyField) ? self::$keyField : self::getDefaultKey();
+
+        foreach (self::all() as $item) {
+            if ($item[$keyField] == $key) {
+                return $item;
+            }
+        }
+
+        return false;
+    }
+
+    public static function getList()
+    {
+        $keyField = isset(self::$keyField) ? self::$keyField : self::getDefaultKey();
+        $valueField = isset(self::$valueField) ? self::$valueField : self::getDefaultValue();
+
+        $list = [];
+        foreach (self::all() as $item) {
+            $list[$item[$keyField]] = $item[$valueField];
+        }
+
+        return $list;
+    }
+
+    protected static function getArrayKeys()
+    {
+        if(!self::$arrayKeys) {
+            self::$arrayKeys = array_keys(self::first());
+        }
+
+        return self::$arrayKeys;
+    }
+
+    protected static function getDefaultKey()
+    {
+        return self::getArrayKeys()[0];
+    }
+
+    protected static function getDefaultValue()
+    {
+        return self::getArrayKeys()[1];
+    }
+
+}
